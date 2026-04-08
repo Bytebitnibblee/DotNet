c89c694ce6b5b2ed24db562ecc5aaf1fe310c27c
Bytebitnibblee <rawaldee32@gmail.com>
2026-04-08 10:37:55 +0545

    Some addition

---
+++ Generic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    internal class Student<T>
+    {
+        T id;
+        public Student(T s_id)
+        {
+            this.id = s_id;
+        }
+            public T getId()
+        { 
+            return this.id;
+        }
+        
+        }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student<int> student = new Student<int>(608);
+            Console.WriteLine(student.getId());
+
+            Student<string> student1 = new Student<string>("Deep Singh Rawal");
+            Console.WriteLine(student1.getId());
+
+            Student<double> student2 = new Student<double> (608.08);
+            Console.WriteLine(student2.getId());
+        }
+    }
+}
+       
---
+++ GenericMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethod
+{
+    public class Student
+    {
+        public static void display<T>(T[] id)
+        {
+            for (int i = 0; i < id.Length; i++)
+            {
+                Console.WriteLine(id[i]);
+            }
+        }
+
+        public class Program
+        {
+            static void Main(string[] args)
+            {
+                int[] nos = new int[3];
+                nos[0] = 101;
+                nos[1] = 102;
+                nos[2] = 103;
+                Student.display(nos);
+
+                string[] names = { "Deep", "Kirtan", "Sangam" };
+                Student.display(names);
+
+                double[] values = { 608.99, 607.99, 608.99 };
+                Student.display(values);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
